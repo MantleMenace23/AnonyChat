09c2126fa38a015bbf3ea1e67f75907ff1797fcc
MantleMenace23 <connor.l.seiple@gmail.com>
2025-08-30 12:12:54 -0400

    hope

--- server.js
+++ server.js
@@ -14,46 +14,47 @@ app.use("/chat", express.static(path.join(__dirname, "public/chat")));
 app.use("/games", express.static(path.join(__dirname, "public/games")));
 app.use("/css", express.static(path.join(__dirname, "public/css")));
 
-// Routes for chat pages
-app.get("/chat", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/chat/index.html"));
-});
-
-app.get("/chat/room", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/chat/chat.html"));
-});
-
-// Routes for games pages
-app.get("/games", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/games/index.html"));
-});
-
-// Default route (redirect to chat lobby)
+// Routes for chat
 app.get("/", (req, res) => {
+    // Lobby
     res.sendFile(path.join(__dirname, "public/chat/index.html"));
 });
 
+app.get("/chat", (req, res) => {
+    // Chat room
+    res.sendFile(path.join(__dirname, "public/chat/chat.html"));
+});
+
+// Routes for games subdomain
+// Render sees the games subdomain as a separate CNAME pointing to same service
+app.get("/", (req, res, next) => {
+    if (req.headers.host.startsWith("games.")) {
+        res.sendFile(path.join(__dirname, "public/games/index.html"));
+    } else {
+        next();
+    }
+});
 
 // Socket.io chat logic
 io.on("connection", (socket) => {
-  console.log("A user connected");
+    console.log("A user connected");
 
-  socket.on("join", (username) => {
-    socket.username = username;
-    socket.broadcast.emit("user-joined", username);
-  });
+    socket.on("join", (username) => {
+        socket.username = username;
+        socket.broadcast.emit("user-joined", username);
+    });
 
-  socket.on("send-message", ({ sender, message }) => {
-    io.emit("receive-message", { sender, message });
-  });
+    socket.on("send-message", ({ sender, message }) => {
+        io.emit("receive-message", { sender, message });
+    });
 
-  socket.on("disconnect", () => {
-    if (socket.username) {
-      socket.broadcast.emit("user-left", socket.username);
-    }
-  });
+    socket.on("disconnect", () => {
+        if (socket.username) {
+            socket.broadcast.emit("user-left", socket.username);
+        }
+    });
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });

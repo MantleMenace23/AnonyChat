859d857a2f2e82467c71c2b04532fe02e260c13a
MantleMenace23 <connor.l.seiple@gmail.com>
2025-08-31 15:32:21 -0400

    *super deep sigh*

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
+const fs = require("fs");
 const { Server } = require("socket.io");
 const multer = require("multer");
-const fs = require("fs");
+const vhost = require("vhost");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// --- Config ---
 const PORT = process.env.PORT || 10000;
 const CHAT_DOMAIN = "anonychat.xyz";
 const GAMES_DOMAIN = "games.anonychat.xyz";
 
-// --- Storage for game uploads ---
+// --- Multer storage for game uploads ---
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, path.join(__dirname, "public/games/game_uploads"));
@@ -25,9 +25,6 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-// --- Virtual hosts ---
-const vhost = require("vhost");
-
 // --- Chat app ---
 const chatApp = express();
 chatApp.use(express.static(path.join(__dirname, "public/chat")));
@@ -35,34 +32,52 @@ chatApp.get("/chat", (req, res) => {
   res.sendFile(path.join(__dirname, "public/chat/chat.html"));
 });
 
-// --- Game app ---
+// --- Games app ---
 const gamesApp = express();
 gamesApp.use(express.static(path.join(__dirname, "public/games")));
-gamesApp.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/games/index.html"));
+
+// Dynamic game list API
+gamesApp.get("/game_list", (req, res) => {
+  const uploadsPath = path.join(__dirname, "public/games/game_uploads");
+  const imagesPath = path.join(uploadsPath, "images");
+
+  const games = fs.readdirSync(uploadsPath)
+    .filter(f => f.endsWith(".html"))
+    .map(file => {
+      const name = path.parse(file).name;
+      // Check for matching image
+      const possibleExtensions = [".png", ".jpg", ".jpeg"];
+      let image = null;
+      for (let ext of possibleExtensions) {
+        const imgPath = path.join(imagesPath, name + ext);
+        if (fs.existsSync(imgPath)) {
+          image = "/game_uploads/images/" + name + ext;
+          break;
+        }
+      }
+      return { name, file: "/game_uploads/" + file, image };
+    });
+  res.json(games);
 });
 
-// Upload game files
+// Upload a new game file
 gamesApp.post("/upload", upload.single("gameFile"), (req, res) => {
   res.json({ success: true, file: req.file.filename });
 });
 
-// --- Use vhost ---
+// --- Mount vhosts ---
 app.use(vhost(CHAT_DOMAIN, chatApp));
 app.use(vhost(GAMES_DOMAIN, gamesApp));
 
-// --- Socket.io for chat ---
-io.on("connection", (socket) => {
-  socket.on("joinRoom", (room) => {
+// --- Socket.IO for chat rooms ---
+io.on("connection", socket => {
+  socket.on("joinRoom", room => {
     socket.join(room);
   });
-
   socket.on("message", ({ room, user, msg }) => {
     io.to(room).emit("message", { user, msg });
   });
 });
 
 // --- Start server ---
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
